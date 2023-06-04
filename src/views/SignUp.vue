49791c0de4628c0d0f9814434a7627e7acdef440
OSarmient <omarcamilo20@gmail.com>
2023-06-03 19:28:49 -0500

    Terminos y condiciones

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-<!--        <NavComponent/>-->
+       <!-- <NavComponent/> -->
         <section class="contenedor">
             <section class="formulario">
                 <form @submit="signUpMethod" >
@@ -20,23 +20,35 @@
                     <div>
                         <input type="email" id="email" placeholder="E-mail" v-model="email" required>
                     </div>
+                    <div class="terms">
+                        <input type="checkbox">
+                        <p>Acepta nuestros <span @click="openModal">Terminos y Condiciones</span>.</p>
+                    </div>
                     <button type="submit">Registrarse</button>
                 </form>
             </section>
+            <Modal class="card-terminos-condiciones" title="Términos & Condiciones" v-model:visible="isVisible">
+                <TerCons/>
+            </Modal>
         </section>
     </section>
 </template>
 
 <script>
 import axios from 'axios'
-/*import NavComponent from '../components/NavComponent.vue'*/
+// import NavComponent from '../components/NavComponent.vue'
+import TerCons from '../components/TerminosCondicionesComponet.vue'
+
+import { Modal } from 'usemodal-vue3';
 
 const requestPath = '/api/sign-up';
 
 export default {
     name: 'SignUp',
     components: {
-        /*NavComponent*/
+        // NavComponent,
+        Modal,
+        TerCons,
     },
     data(){
         return{
@@ -44,7 +56,8 @@ export default {
             password: '',
             repeatPassword: '',
             name: '',
-            email: ''
+            email: '',
+            isVisible: false,
         }
     },
     methods:{
@@ -77,15 +90,44 @@ export default {
                     alert("Error de servidor")
                 }
             });
-        event.preventDefault();
-        return true;
+            event.preventDefault();
+            return true;
+        },
+
+        async openModal(){
+            this.isVisible = true;
         }
     },
 };
 </script>
 
 <style scoped>
-div {
-  width: 100%;
-}
+    div {
+        width: 100%;
+    }
+    div.terms {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    div.terms input{
+        width: auto;
+    }
+      
+    div.terms p {
+        margin: 10px;
+        align-items: center;
+    }
+      
+     div.terms p span {
+        cursor: pointer;
+        color: orange;
+     }
+
+     .card-terminos-condiciones{
+        position: fixed;
+        color: black;
+     }
+     
 </style>
